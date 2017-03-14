9e0e892bc23a4b0bc7c5b16c4233ef190444f63c
austi <austi@LAPTOP-TDC2Q0VM>
2017-03-13 21:26:16 -0700

    Add project files.

---
+++ GPXReader/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string XMLDocPath = (args[0]);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(XMLDocPath);
+
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                //string text = node.InnerText; //or loop through its children as well
+
+                string attr = node.Attributes["trkpt"]?.InnerText;
+
+                string text = node.InnerXml;
+
+                //System.Console.WriteLine(attr);
+                System.Console.WriteLine(text);
+            }
+        }
+    }
+}
